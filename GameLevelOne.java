8ba54bc5b618765689617345448e8e84baef54d1
JuanAV21 <75455770+JuanAV21@users.noreply.github.com>
2021-04-07 15:51:19 -0400

    Health

---
+++ GameLevelOne.java
@@ -0,0 +1,31 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Game here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GameLevelOne extends World
+{
+    /**
+     * Constructor for objects of class Game.
+     * 
+     */
+    public GameLevelOne()
+    {    
+        super(800, 600, 1);
+        
+        Back back = new Back();
+        addObject(back,739,543);
+        
+        Character test = new Character();
+        addObject(test,566,268);
+
+        Label label = new Label();
+        addObject(label,466,268);
+        
+        Health health = new Health();
+        addObject(health,466,318);
+    }
+}
