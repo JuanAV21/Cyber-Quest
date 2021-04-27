084af3d7d2549a84a0e3f2d5dfa495a3a3f034a6
Joshua Heredia <jjheredia19@students.desu.edu>
2021-04-27 19:19:15 -0400

    Animation

--- minion.java
+++ minion.java
@@ -8,22 +8,27 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class minion extends Actor
 {
+    int animationCounter = 0;
+    private int speed = 1;
     /**
      * Act - do whatever the Minion wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public minion(){
-        GreenfootImage myImage = new GreenfootImage("idle1.png");
-        int myNewHeight =(int)myImage.getHeight()*2;
-        int myNewWidth =(int)myImage.getWidth()*2;
-        myImage.scale(myNewWidth, myNewHeight);
-        setImage(myImage);
+      GreenfootImage myImage = new GreenfootImage("idle1.png");
+      int myNewHeight =(int)myImage.getHeight()*2;
+      int myNewWidth =(int)myImage.getWidth()*2;
+      myImage.scale(myNewWidth, myNewHeight);
+      setImage(myImage); 
     }
     public void act() 
     {
-        move(1);
+        move(speed);
         Enemymove();
         die();
+        if(animationCounter % 89 == 0){
+            EnemyAnimation();
+        }
     }
     
     public void Enemymove()
@@ -45,7 +50,45 @@ public class minion extends Actor
         {
            getWorld().removeObject(bullet);
            getWorld().removeObject(this);
+        } 
+    }
+    
+    int frame = 1;
+    public void EnemyAnimation()
+    {
+        if(frame == 1)
+        {
+            setImage("walk"+1+".png");
         }
-           
-    }  
+        else if(frame == 2)
+        {
+            setImage("walk"+2+".png");
+        }
+        else if(frame == 3)
+        {
+            setImage("walk"+3+".png");
+        }
+        else if(frame == 4)
+        {
+            setImage("walk"+4+".png");
+        }
+        else if(frame == 5)
+        {
+            setImage("walk"+5+".png");
+        }
+        else if(frame == 6)
+        {
+            setImage("walk"+6+".png");
+        }
+        else if(frame == 7)
+        {
+            setImage("walk"+7+".png");
+        }
+        else if(frame == 8)
+        {
+            setImage("walk"+8+".png");
+            frame = 1;
+        }
+        frame ++;
+    }
 }
