a60ae3b677e8bfbb2e6aa2fe5b90a02bf5a3bfaf
davidkpognon <dskpognon20@students.desu.edu>
2021-05-07 11:20:55 -0400

    David

--- MyWorld.java
+++ MyWorld.java
@@ -1,21 +1,22 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * Write a description of class Myworld here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class MyWorld extends World
+public class Myworld extends World
 {
+
     /**
-     * Constructor for objects of class MyWorld.
+     * Constructor for objects of class Myworld.
      * 
      */
-    public MyWorld()
+    public Myworld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(800, 600, 1,false); 
+        super(600, 400, 1); 
         prepare();
     }
 
@@ -25,13 +26,17 @@ public class MyWorld extends World
      */
     private void prepare()
     {
-        Credits credits = new Credits();
-        addObject(credits,652,489);
-        HowToPlay howToPlay = new HowToPlay();
-        addObject(howToPlay,555,357);
-        Start start = new Start();
-        addObject(start,647,224);
-        Title title = new Title();
-        addObject(title,394,72);
+        Character1 character1 = new Character1();
+        addObject(character1,154,99);
+        character1.setLocation(293,141);
+        Fish[] fish = new Fish[8];
+        for(int i = 0; i < fish.length; i++)
+        {
+           fish[i] = new Fish();
+           int fishX = Greenfoot.getRandomNumber(getWidth());
+           int fishY = Greenfoot.getRandomNumber(getHeight());
+           addObject(fish[i],fishX , fishY);
+        }
+        
     }
 }
